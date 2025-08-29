839ee4e5c231a6552ac998c019ec0943e493a4b9
Farhan Yousafzai <coderwithferry@gmail.com>
2025-08-30 03:15:11 +0500

    Start

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center bg-blue-700 h-screen w-screen'>
+      
+    </div>
+  )
+}
+
+export default App
